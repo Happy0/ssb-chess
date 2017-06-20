1c0715eca179b62d3b2f4e06e7b0ce043d2a6f2f
Gordon Martin <gordon.hugh.martin@gmail.com>
2017-06-20 19:40:11 +0100

    Let's just stick with the default ssb key location since that's what the thing appears to be doing anyway :P. inb4 accidentally spamming the scuttlebutt network with test chess move messages.

--- main.js
+++ main.js
@@ -1,23 +1,14 @@
 var fs = require('fs');
-var ssbKeys = require('ssb-keys');
 var ssbClient = require('ssb-client')
-
-var homeDirectory = require('os').homedir();
-var ssbChessDirectory = homeDirectory + "/.ssb-chess/";
-
-var ssbChessKeys = ssbChessDirectory + "keys";
-var ssbDb = ssbChessDirectory + "/db";
 
 var conf = require("./conf/config.json");
 
-// TODO: parameterise path or just use the ssb default
-var keys = ssbKeys.loadOrCreateSync(ssbChessKeys);
 
 var GameCtrl = require('./ctrl/game');
 
 var CommandLine = require("./command_line")
 
-ssbClient(keys,
+ssbClient(
   function (err, sbot) {
     if (err) {
       console.log(err);
@@ -26,15 +17,6 @@ ssbClient(keys,
     sbot.whoami((err,ident) => {
       const gameCtrl = GameCtrl(sbot, ident.id);
       const commandHandler = CommandLine(gameCtrl);
-
-      // console.log("dfsfdsf");
-      // pull(
-      //   sbot.createFeedStream(),
-      //   pull.collect(function (err, ary) {
-      //     console.log("yoyo");
-      //     console.log(ary)
-      //   })
-      // )
     })
   });
 
