9ef787ebd31d4fa4219bfbcfce00af015a03c3bc
Gordon Martin <gordon.hugh.martin@gmail.com>
2017-06-25 02:03:50 +0100

    Better module name

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var ssbClient = require('ssb-client');
 var GameCtrl = require('./ctrl/game');
 var m = require("mithril");
 
-var MiniboardListComponent = require('./ui/overview/miniboard_list');
+var MiniboardListComponent = require('./ui/miniboard/miniboard_list');
 
 module.exports = () => {
 
@@ -14,7 +14,6 @@ module.exports = () => {
         console.log("Starting ssb-chess");
         sbot.whoami((err,ident) => {
           const gameCtrl = GameCtrl(sbot, ident.id);
-
           const gamesAwaitingMove = MiniboardListComponent(gameCtrl.getMyGamesInProgress);
 
           m.mount(document.body, gamesAwaitingMove);
