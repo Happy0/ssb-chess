bd3e014de5dd5fa5f20b864eebaea2816a35c40a
Gordon Martin <gordon.hugh.martin@gmail.com>
2017-07-19 21:58:59 +0100

    Fix miniboards flickering when rendering issue :D

--- ui/miniboard/miniboard.js
+++ ui/miniboard/miniboard.js
@@ -6,62 +6,75 @@ module.exports = (summary, identPerspective) => {
 
   var chessground = null;
 
+  // An observer might not be in the 'players' list so we need a default
+  // perspective of white for them.
+  const playerColour = (summary.players[identPerspective] &&
+    summary.players[identPerspective].colour) ? summary.players[identPerspective].colour : "white";
+
   function renderSummary() {
-
-    // An observer might not be in the 'players' list so we need a default
-    // perspective of white for them.
-    const playerColour = (summary.players[identPerspective] &&
-       summary.players[identPerspective].colour) ? summary.players[identPerspective].colour: "white";
-
-    var vDom = m('a[href=' + '/games/' + btoa(summary.gameId) +']', {class: 'cg-board-wrap', title: summary.gameId, oncreate: m.route.link},
-      m("div", {
-        id: summary.gameId
-      }))
-
-    var config = {
-      fen: summary.fen,
-      viewOnly: true,
-      orientation: playerColour
-    };
-
-    if (summary.lastMove) {
-      config.lastMove = [summary.lastMove.orig, summary.lastMove.dest];
-    }
-
-    // The dom element isn't available yet
-    setTimeout(() => {
-      var element = vDom.dom;
-      chessground = Chessground(element, config);
-    });
 
     var coloursNames = PlayerModelUtils.coloursToNames(summary.players);
     var otherPlayerColour = playerColour == "white" ? "black" : "white";
 
     return m('div', {
-        class: "ssb-chess-miniboard blue merida"
-      }, [m('center', {class: "ssb-chess-miniboard-name"}, coloursNames[otherPlayerColour].substring(0, 10)),
-      vDom,
-      m('center', {class: "ssb-chess-miniboard-name"}, coloursNames[playerColour].substring(0, 10))]);
+      class: "ssb-chess-miniboard blue merida"
+    }, [m('center', {
+        class: "ssb-chess-miniboard-name"
+      }, coloursNames[otherPlayerColour].substring(0, 10)),
+      m('a[href=' + '/games/' + btoa(summary.gameId) + ']', {
+        class: "ssb-chessground-container",
+        title: summary.gameId,
+        id: summary.gameId,
+        oncreate: m.route.link
+      }),
+      m('center', {
+        class: "ssb-chess-miniboard-name"
+      }, coloursNames[playerColour].substring(0, 10))
+    ]);
+  }
+
+  function listenForLiveGameUpdates() {
+    this.moveListener = PubSub.subscribe("game_update", function(msg, data) {
+      if (data.gameId === summary.gameId) {
+        var config = {
+          fen: data.fen,
+          lastMove: [data.orig, data.dest]
+        }
+
+        if (chessground) {
+          chessground.set(config);
+        }
+      }
+    });
   }
 
   return {
     view: function() {
       return renderSummary();
     },
+    oncreate: function(vNode) {
+
+      // This lifecycle event tells us that the DOM is ready. That means we
+      // can attach chessground to our chessground container element that was
+      // prepared for it during the 'view' lifecycle method.
+
+      var config = {
+        fen: summary.fen,
+        viewOnly: true,
+        orientation: playerColour
+      };
+
+      if (summary.lastMove) {
+        config.lastMove = [summary.lastMove.orig, summary.lastMove.dest];
+      }
+
+      var dom = vNode.dom;
+      var chessGroundParent = dom.querySelector(".ssb-chessground-container");
+      chessground = Chessground(chessGroundParent, config);
+
+    },
     oninit: function() {
-      this.moveListener = PubSub.subscribe("game_update", function(msg, data) {
-        if (data.gameId === summary.gameId) {
-          var config = {
-            fen: data.fen,
-            lastMove: [data.orig, data.dest]
-          }
-
-          if (chessground) {
-              chessground.set(config);
-          }
-        }
-      });
-
+      listenForLiveGameUpdates();
     },
     onremove: function() {
       PubSub.unsubscribe(this.moveListener);
