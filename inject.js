379696be3dfdae6e8309e9870ddbd38e24559008
mixmix <whimful@gmail.com>
2018-07-16 20:16:37 +1200

    get dat chess routing working

--- inject.js
+++ inject.js
@@ -8,6 +8,7 @@ const index = require('./index');
 
 exports.gives = nest({
   'app.html.menuItem': true,
+  'app.sync.locationId': true,
   'router.sync.routes': true,
 })
 
@@ -17,13 +18,13 @@ exports.needs = nest({
 });
 
 exports.create = function(api) {
-
   var pageLoaded = false;
-  var topLevelDomElement = null;
+  var topLevelDomElement = ChessContainer()
   var app = null;
 
   return nest({
     'app.html.menuItem': menuItem,
+    'app.sync.locationId': locationId,
     'router.sync.routes': routes,
   })
 
@@ -36,15 +37,11 @@ exports.create = function(api) {
     }, '/chess')
   }
 
-  function chessIndex(location) {
-
+  function chessIndex() {
     // We only create the app once (for now.)
     if (pageLoaded) {
-      topLevelDomElement.title = `/chess`;
-      topLevelDomElement.id = api.app.sync.locationId(location);
       return topLevelDomElement;
     } else {
-      topLevelDomElement = ChessContainer(location);
       pageLoaded = true;
 
       onceTrue(api.sbot.obs.connection, (sbot) => {
@@ -56,22 +53,19 @@ exports.create = function(api) {
   }
 
   function chessShow(location) {
-
     const gameId = location.key;
 
     // If the app is already open, route to the game, otherwise open the app at the
     // page
     if (pageLoaded) {
       app.goToGame(gameId);
+      // TODO - put this in the router?
       topLevelDomElement.title = `/chess/${getRoot(location)}`;
-      topLevelDomElement.id = api.app.sync.locationId(location);
 
       return topLevelDomElement;
     } else {
-
       var destinationRoute = `/games/${btoa(gameId)}`;
 
-      topLevelDomElement = ChessContainer(location);
       pageLoaded = true;
 
       onceTrue(api.sbot.obs.connection, (sbot) => {
@@ -95,16 +89,24 @@ exports.create = function(api) {
   }
 
 
-  function ChessContainer (location) {
+  function ChessContainer () {
     const root = h('div.ssb-chess-container')
-    root.title = location.page
-      ? '/chess'
-      : `/chess/${getRoot(location)}`
+    // root.title = location.page
+      // ? '/chess'
+      // : `/chess/${getRoot(location)}`
 
-    root.id = api.app.sync.locationId(location)
+    // root.id = api.app.sync.locationId(location)
+    
+    root.title = '/chess'
+    root.id = JSON.stringify({ page: 'chess' })
 
     return root
   }
+}
+
+function locationId (location) {
+  if (location.page === 'chess') return JSON.stringify({ page: 'chess' })
+  if (isChessMsg(location)) return JSON.stringify({ page: 'chess' })
 }
 
 function isChessMsg (loc) {
