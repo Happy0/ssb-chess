57bb356503c1135fef8f4ea9c0e5159ddf0adc80
Gordon Martin <gordon.hugh.martin@gmail.com>
2018-10-13 11:30:08 +0100

    Fix issue with ssb-chess-db plugin name.

--- ctrl/main.js
+++ ctrl/main.js
@@ -51,6 +51,20 @@ module.exports = (sbot, myIdent) => {
     return myIdent;
   }
 
+  function setUpGamesIndex() {
+    // In older versions of ssb-chess-db, the plugin was given 'ssbChessIndex'
+    // as a name. This caused issues when loading the plugin into a standalone
+    // scuttlebot ( https://github.com/Happy0/ssb-chess-db/issues/1 )
+    // We deal with this old name for backwards compatibility.
+    if (sbot['chess-db']) {
+      sbot.ssbChessIndex = sbot['chess-db'];
+    } else if (!sbot.ssbChessIndex && !sbot['chess-db']) {
+      throw new Error('Missing plugin ssb-chess-db');
+    }
+  }
+
+  setUpGamesIndex();
+
   return {
     /**
      * Get the player's public key.
